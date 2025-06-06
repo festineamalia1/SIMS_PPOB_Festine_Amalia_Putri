a520a518f68fd7c4d8b4fac30148b10a132d51e4
DESKTOP-8KUTOID\user <festineamalia6@gmail.com>
2025-06-06 19:33:38 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import Switch from "react-bootstrap/esm/Switch";
 import PrivateRoute from "./components/PrivateRoute";
 import 'bootstrap/dist/css/bootstrap.css';
  import { HashRouter as Router, Route, BrowserRouter, Routes } from "react-router-dom";
-// import {
-//   BrowserRouter,
-//   Routes, // <-- add import
-//   Route,
-// } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
 import Home from "pages/Home";
 import TopUp from "pages/TopUp";
