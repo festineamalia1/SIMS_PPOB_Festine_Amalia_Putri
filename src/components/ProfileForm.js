a456bc0528e8b00efaa2c7e01831e2e4b98e0bfc
DESKTOP-8KUTOID\user <festineamalia6@gmail.com>
2025-06-12 22:54:43 +0700

    update

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -5,40 +5,104 @@ import { Link, useNavigate  } from "react-router-dom";
 import { API, setAuthToken } from "config/api";
 import { connect } from "react-redux";
 import { handleLogin } from "actions";
-const RegisterForm = (props) => {
+import axios from "axios";
+const ProfileForm = ({dataProfile}) => {
   const navigate = useNavigate ();
-  // const handleLogin = () => {
-  //   history.push(`/home`);
-  // };
+   const [edit, setEdit] = useState(false);
+ const TOKEN = localStorage.getItem('token')
+  const [userEmail, setUserEmail] = useState(dataProfile?.data?.data?.email);
+  const [firstName, setFirstName] = useState(dataProfile?.data?.data?.first_name);
+  const [lastName, setLastName] = useState(dataProfile?.data?.data?.last_name);
 
-  const [userName, setUserName] = useState("admin");
-  const [password, setPasword] = useState("admin");
+  console.log("dataProfileEdit Email", userEmail);
+  console.log("dataProfileEdit first", firstName);
+  console.log("dataProfileEdit last", lastName);
 
-  console.log("statusLog", props.statusLog);
-
-  // useEffect(() => {
-  //   props.onHandleLogin()
-  // }, []);
-  const StatusLog = window.localStorage.getItem("LogStatus");
-  const handleRedirect = () => {
-    window.location.reload();
+const handleEditProfile = (e) => {
+       e.preventDefault();
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      'Authorization': `Bearer ${TOKEN}`
+    };
+    axios
+      .put(
+        `${API}/profile/update`,
+       {
+         first_name: firstName,
+          last_name: lastName
+},
+        {
+          headers: headers,
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+        alert("Berhasil di bayar");
+        window.location.reload();
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert(
+          error.response.data.message
+        );
+      });
   };
+  const StatusLog = window.localStorage.getItem("LogStatus");
+
   return (
    <>
     {/* <div class="col d-flex align-items-center justify-content-center mx-8">Nama Profile</div> */}
    <div class="form-heading d-flex align-items-center justify-content-center mx-8">Nama Profile</div>
-
+  <div className="row mt-5">
      <form className="px-5">
             <div className="mb-3">
-              <input type="email" className="form-control" placeholder="Masukkan email anda"/>
+              <input type="email" className="form-control" placeholder="Masukkan email anda"
+              value={userEmail}
+              onChange={(e) => {setUserEmail(e.target.value)}}
+              />
             </div>
             <div className="mb-3">
-              <input type="text" className="form-control" placeholder="Nama depan"/>
+              <input type="text" className="form-control" placeholder="Nama depan"
+                 value={firstName}
+              onChange={(e) => {setFirstName(e.target.value)}}
+              />
             </div>
             <div className="mb-3">
-              <input type="text" className="form-control" placeholder="Nama belakang"/>
+              <input type="text" className="form-control" placeholder="Nama belakang"
+              value={lastName}
+              onChange={(e) => {setLastName(e.target.value)}}
+              />
             </div>
           </form>
+          </div>
+
+            
+              {
+                edit == true ?
+                 <div className="row px-5">
+                  <button type="button" 
+                  class="btn btn-outline-danger w-100"
+                  onClick={(e) => {handleEditProfile(e)}}
+                  >Simpan</button>
+                  </div>
+                  :
+                  <>
+                   <div className="row px-5">
+                  <button type="button" 
+                  class="btn btn-outline-danger w-100"
+                  onClick={() => {setEdit(true)}}
+                  >Edit Profile</button>
+                  </div>
+                    <div className="row mt-3 px-5">
+           <button type="submit" className="btn btn-danger w-100">Logout</button>
+        </div>
+                  </>
+              }
+           
+             
+      
+       
+
    </>
     
           
@@ -54,4 +118,4 @@ const mapDispatchprops = (dispatch) => {
   return { onHandleLogin: () => dispatch(handleLogin()) };
 };
 
-export default connect(mapStatetoProps, mapDispatchprops)(RegisterForm);
+export default connect(mapStatetoProps, mapDispatchprops)(ProfileForm);
--- src/pages/AkunPage.js
+++ src/pages/AkunPage.js
@@ -20,201 +20,48 @@ import axios from "axios";
 // import Barang from "../assets/images/barang1.jpg";
 
 export default function AkunPage() {
-  const [query, setQuery] = useState("");
-  const [show, setShow] = useState(false);
-  const [show2, setShow2] = useState(false);
-  const [show3, setShow3] = useState(false);
-  const [show4, setShow4] = useState(false);
-
-  const [idDelete, setIdDelete] = useState();
-  const [showDelete, setShowDelete] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleClose2 = () => setShow2(false);
-  const handleClose3 = () => setShow3(false);
-  const handleClose4 = () => setShow4(false);
-  const handleOpen = () => setShow(true);
-  const handleOpen2 = (id) => {
-    setShow2(true);
-    setIdDelete(id);
-  };
-  const handleOpen3 = (id) => {
-    setShow3(true);
-  };
-  const handleOpen4 = (id) => {
-    setShow4(true);
-    setIdDelete(id);
-  };
+ const TOKEN = localStorage.getItem('token')
 const navigate = useNavigate();
 
-  const [userId, setUserId] = useState("");
-  const [userDataDetail, setUserDataDetail] = useState("");
- const [Edit, setEdit] = useState(false);
-  const handleShow = (id) => {
-    handlDetailBarang(id);
-    setShow(true);
-  };
-  console.log("userId", userId);
+
   const [userData, setUserData] = useState([]);
-
-  const [barangData, setBarangData] = useState([]);
-  const [detailBarang, setDetailBarang] = useState([]);
+ const [files, setFiles] = useState();
+ const [loading, setIsLoading] = useState([]);
  
-  const [nama, setNama] = useState();
-  const [foto, setFoto] = useState();
-  const [beli, setBeli] = useState();
-  const [jual, setJual] = useState();
-  const [stok, setStok] = useState();
-  const [admin, setAdmin] = useState();
-  const [searchData, setSearchData] = useState();
-
-  const namaBrgEdit = window.localStorage.getItem("Nama");
-  const fotoBrgEdit = window.localStorage.getItem("Nama");
-  const beliBrgEdit = window.localStorage.getItem("Beli");
-  const jualBrgEdit = window.localStorage.getItem("Jual");
-  const stokBrgEdit = window.localStorage.getItem("Stok");
-
-  const [namaEdit, setNamaEdit] = useState("");
-  const [fotoEdit, setFotoEdit] = useState("");
-  const [beliEdit, setBeliEdit] = useState(0);
-  const [jualEdit, setJualEdit] = useState(0);
-  const [stokEdit, setStokEdit] = useState(0);
-  const [adminEdit, setAdminEdit] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
-  console.log("beliEdit", beliEdit);
-
-  const fetchDataBarang = () => {
+  const fetchDataAkun = () => {
     setIsLoading(true);
     axios
-      .get(`${API}/barang`)
-      .then(function (response) {
-        setIsLoading(false);
-        console.log(response);
-        setBarangData(response.data.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
-  const handleSearchBarang = () => {
-    setIsLoading(true);
-    axios
-      .get(`${API}/search/${searchData}`)
-      .then(function (response) {
-        setIsLoading(false);
-        console.log(response);
-        setBarangData(response.data.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
-  const handlDetailBarang = (id) => {
-    axios
-      .get(`${API}/barang/${id}`)
-      .then(function (response) {
-        console.log(response);
-        setDetailBarang(response.data.data[0]);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
-  const handleTambah = () => {
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
-    axios
-      .post(
-        `${API}/barang`,
+      .get(`${API}/profile`, 
         {
-          nama_barang: nama,
-          foto_barang: "barang5.jpg",
-          harga_beli: beli,
-          harga_jual: jual,
-          stok: stok,
-          admin: 1,
-        },
-        {
-          headers: headers,
-        }
-      )
+    headers: {
+        'Authorization': `Bearer ${TOKEN}`
+    }
+}
+)
       .then(function (response) {
-        console.log(response);
-        alert("tambah Data Berhasil");
-        window.location.reload();
+       setIsLoading(false);
+       setUserData(response)
       })
       .catch(function (error) {
         console.log(error);
-        alert(
-          "tambah Data Gagal, Nama Barang Pastikan berbeda, untuk harga beli, harga jual dan stok hanya dapat di isi angka"
-        );
       });
   };
-
-  const handleEdit = () => {
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
-    axios
-      .post(
-        `${API}/barang/${idDelete}`,
-        {
-          nama_barang: namaEdit,
-          foto_barang: "barang5.jpg",
-          harga_beli: beliEdit,
-          harga_jual: jualEdit,
-          stok: stokEdit,
-          admin: 1,
-        },
-        {
-          headers: headers,
-        }
-      )
-      .then(function (response) {
-        console.log(response);
-        alert("Edit Data Berhasil");
-        window.location.reload();
-      })
-      .catch(function (error) {
-        console.log(error);
-        alert(
-          "Edit Data Gagal. Nama Barang Pastikan berbeda, untuk harga beli, harga jual dan stok hanya dapat di isi angka"
-        );
-      });
-  };
-
-  const handleDelete = () => {
-    axios
-      .post(`${API}/barang/${idDelete}`)
-      .then(function (response) {
-        console.log(response);
-        alert("hapus Data Berhasil");
-        window.location.reload();
-      })
-      .catch(function (error) {
-        console.log(error);
-        alert("hapus Data Gagal");
-      });
-  };
-
-  const [files, setFiles] = useState();
+ 
+console.log("userData", userData)
+ 
 
   const onChangeFiles = (e) => {
     console.log("file", e.target.files[0]);
     setFiles(e.target.files[0]);
   };
 
-  console.log("barangData", barangData);
+
+ 
   useEffect(() => {
-    fetchDataBarang();
+ fetchDataAkun()
   }, []);
 
-  console.log("searchData", searchData);
+
 
   return (
     <>
@@ -225,38 +72,26 @@ const navigate = useNavigate();
 
     <section id="profile">
       <div className="container">
-        <div className="row mt-5">
- 
-     <ProfileForm/>
-
-
+        <div className="row">
+          <div className="avatar-container">
+  <img src={userData?.data?.data?.profile_image} alt="Avatar" className="avatar-img"/>
+  <div className="edit-icon">
+    <i className="bi bi-pencil-fill"></i>
+  </div>
+</div>
         </div>
-        {
-          Edit == false ?
-          <>
-          <div className="row px-5">
-           <button type="button" 
-           class="btn btn-outline-danger w-100"
-           onClick={() => {setEdit(true)}}
-           >Edit Profile</button>
-             
-        </div>
-         <div className="row mt-3 px-5">
-           <button type="submit" className="btn btn-danger w-100">Logout</button>
-        </div>
-        </>
+      {
+        loading == true ?
+        "Sebentar"
         :
-        <div className="row px-5">
-           <button type="button" 
-           class="btn btn-outline-danger w-100"
-           onClick={() => {
-                    navigate('/home');
-                }}
-           >Simpan</button>
-             
-        </div>
+      <ProfileForm dataProfile={userData}/>
+      }
+ 
+  
 
-        }
+
+
+
           
        
      
